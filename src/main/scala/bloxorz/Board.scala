0d3143db333420b9272204a2bdd37e5e68443b8d
Dimitrije Glisic <52169396+dimitrije-glisic@users.noreply.github.com>
2022-08-21 21:31:07 +0200

    Create companion Object for Board

--- src/main/scala/bloxorz/Board.scala
+++ src/main/scala/bloxorz/Board.scala
@@ -1,5 +1,23 @@
 package bloxorz
 
+import bloxorz.Constants.{START}
+
+object Board{
+  def apply(matrix: List[Array[Char]]): Board = {
+    new Board(matrix)
+  }
+
+  def getInitPosition(matrix: List[Array[Char]]): (Int, Int) = {
+    var result = (-1, -1)
+    for ((row, i) <- matrix.view.zipWithIndex if row.contains(START)) result = (i, row.indexOf(START))
+    result
+  }
+
+  def getFieldValue(matrix: List[Array[Char]], cursor: Cursor):Char = {
+    matrix.take(cursor.row + 1).last(cursor.col)
+  }
+}
+
 class Board(var matrix: List[Array[Char]]) {
 
   val matrix_m:Int = {
@@ -9,10 +27,5 @@ class Board(var matrix: List[Array[Char]]) {
   val matrix_n:Int = {
     matrix.take(1).last.length
   }
-
-  def getFieldValue(cursor: Cursor):Char = {
-    matrix.take(cursor.row + 1).last(cursor.col)
-  }
-
 
 }
--- src/main/scala/bloxorz/MapEditor.scala
+++ src/main/scala/bloxorz/MapEditor.scala
@@ -95,19 +95,19 @@ class MapEditor(val board: Board) {
   }
 
   def canBeReplacedWithDash(cursor: Cursor): Boolean = {
-    board.getFieldValue(cursor) == Constants.REGULAR_VALUE && isOnTheEdge(cursor)
+    Board.getFieldValue(board.matrix, cursor) == Constants.REGULAR_VALUE && isOnTheEdge(cursor)
   }
 
   def canBeReplacedWithRegular(cursor: Cursor): Boolean = {
-    board.getFieldValue(cursor) == Constants.DASH && isOnTheEdge(cursor)
+    Board.getFieldValue(board.matrix, cursor) == Constants.DASH && isOnTheEdge(cursor)
   }
 
   def isOnTheEdge(cursor: Cursor): Boolean = {
     val (i, j) = (cursor.row, cursor.col)
-    val left = if (j - 1 > 0) board.getFieldValue(new Cursor(i, j - 1)) else DASH
-    val right = if (j < board.matrix_n - 1) board.getFieldValue(new Cursor(i, j + 1)) else DASH
-    val above = if (i - 1 > 0) board.getFieldValue(new Cursor(i - 1, j)) else DASH
-    val below = if (i + 1 < board.matrix_m) board.getFieldValue(new Cursor(i + 1, j)) else DASH
+    val left = if (j - 1 > 0) Board.getFieldValue(board.matrix, new Cursor(i, j - 1)) else DASH
+    val right = if (j < board.matrix_n - 1) Board.getFieldValue(board.matrix, new Cursor(i, j + 1)) else DASH
+    val above = if (i - 1 > 0) Board.getFieldValue(board.matrix, new Cursor(i - 1, j)) else DASH
+    val below = if (i + 1 < board.matrix_m) Board.getFieldValue(board.matrix, new Cursor(i + 1, j)) else DASH
     thereIsOneDash(left, right, above, below) && thereIsOneNonDash(left, right, above, below)
   }
 
